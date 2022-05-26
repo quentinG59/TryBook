db561e6dcbc7db27567b1220fac7b05cc7443ae6
Gibert Quentin <quentingibert6@gmail.com>
2022-05-26 14:26:51 +0200

    first commit front

---
+++ src/pages/Profil.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Log from "../components/Log";
+
+const Profil = () => {
+  return (
+    <div className="profil-page">
+      <div className="log-container">
+        <Log signin={false} signup={true} />
+        <div className="img-container">
+          <img src="./img/log.svg" alt="img-log" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profil;
---
+++ src/pages/Trending.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Trending = () => {
+  return <div>yo depuis trending</div>;
+};
+
+export default Trending;
