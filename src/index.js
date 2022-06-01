3e5a0ffb4c9999f7fd7bed877445991c9d4c6538
Gibert Quentin <quentingibert6@gmail.com>
2022-06-01 13:24:39 +0200

    debug

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ import App from "./App";
 import "./styles/index.scss";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 //dev tools
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 
-const store = configureStore(
-  { reducer: rootReducer },
-  composeWithDevTools(applyMiddleware(thunk, logger))
-);
+const store = configureStore({
+  reducer: {},
+  middleware: [thunk, logger],
+  composeWithDevTools,
+  preloadedState: {
+    rootReducer: {
+      userReducer: null,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
